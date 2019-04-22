fd9ace1600fdd08be9f0baca880e52c05ab72a26
KyrieJK <18910599692@163.com>
2019-04-23 00:59:48 +0800

    2019/04/23--工具方法，str2ip、parse_ip_port方法具体实现。注意char*、short*等指针的使用，容易引发segmentfault

--- utils/lib.c
+++ utils/lib.c
@@ -2,8 +2,50 @@
 // Created by JKerving on 2019/4/22.
 //
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "lib.h"
 
 int min(int x,int y){
     return x > y ? y : x;
+}
+
+/**
+ * 字符串转换为IP地址
+ * @param str
+ * @param ip
+ * @return
+ */
+int str2ip(char *str, unsigned int *ip)
+{
+    unsigned int a, b, c, d;
+    if (sscanf(str, "%u.%u.%u.%u", &a, &b, &c, &d) != 4)
+        return -1;
+    if (a > 255 || b > 255 || c > 255 || d > 255)
+        return -1;
+    *ip = a | (b << 8) | (c << 16) | (d << 24);
+    return 0;
+}
+
+/**
+ * 解析IP地址中的端口号
+ * @param str
+ * @param addr
+ * @param nport
+ * @return
+ */
+int parse_ip_port(char *str, unsigned int *addr, unsigned short *nport){
+    char *port=NULL;
+    int n=0;
+    nport = (unsigned short*)malloc(sizeof(unsigned short));
+    if((port=strchr(str,':'))!=NULL){
+        n = atoi(&port[1]);
+        *nport = n;
+    }
+
+    if(str2ip(str,addr)<0)
+        return -1;
+
+    return 0;
 }
